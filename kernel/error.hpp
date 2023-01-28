7c42acac0e5665fff284c17e6770d302c3a03a7a
yakishamo <82935423+yakishamo@users.noreply.github.com>
2023-01-28 22:50:47 +0900

    1/28

--- kernel/error.hpp
+++ kernel/error.hpp
@@ -30,7 +30,7 @@ class Error {
   };
 
  private:
-  static constexpr char const *code_names_[20] {
+  static constexpr std::array<const char *, 20> code_names_ {
     "kSuccess",
     "kFull",
     "kEmpty",
@@ -67,7 +67,7 @@ class Error {
   }
 
   const char* Name() const {
-    return code_names_[static_cast<int>(this->code_)];
+    return code_names_[static_cast<size_t>(this->code_)];
   }
 	
   const char* File() const {
